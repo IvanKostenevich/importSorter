4e7a6c621a3cf937979d35593243b892d64133a8
a.parkhimchyk <a.parkhimchyk@itechart-group.com>
2017-01-09 12:49:36 +0300

    Push to my repository to trigger my first build

---
+++ karma.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by a.parkhimchyk on 06.01.2017.
+ */
+
+module.exports = function (config) {
+    config.set({
+        frameworks: ['mocha'],
+        browsers: ['Chrome'],
+        files: [
+            './node_modules/lodash/lodash.min.js',
+            'https://cdn.rawgit.com/Automattic/expect.js/0.3.1/index.js',
+            'sort-imports.js',
+            'test.js'
+        ],
+        singleRun: true
+    });
+};
--- sort-imports.js
+++ sort-imports.js
@@ -2,6 +2,28 @@
 
 /*global _:true*/
 
+var samples = {
+	sampleText1: `/*eslint require-yield:0*/
+'use strict';
+const config = require('../../config.js');
+// eslint-disable-next-line no-unused-vars
+const coMocha = require('co-mocha');
+// eslint-disable-next-line no-unused-vars
+const should = require('chai').should();
+// eslint-disable-next-line no-unused-vars
+const _ = require('lodash');
+const EventLogger = require('@alphaflow/sc-eventstore/lib/event-logger.js');
+const constants = require('@alphaflow/sc-common').constants;
+const HTTPStatus = require('http-status');
+const baseUrl = config.AsdsdasdPP.TEST.BASE_URL;
+const token = require('./../util/jwt-token.js').get(['earning:post']);
+const baseUrl = config.APP.TEST.BASE_URL;
+const request = require('co-request');
+const mock = require('../../mocks.js');
+
+const baseUrl = config.APP.TEST.dfsdfsdfsfBASE_URL;`
+};
+
 var splitIntoLines = function (text) {
 	if (!_.isString(text)) {
 		throw new Error('Invalid argument');
