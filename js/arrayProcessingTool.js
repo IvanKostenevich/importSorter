00c751eeecb4e8322b010777fb2b6d521483c150
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-12 15:24:52 +0300

    implemented getMax/Min/MediumValue functions, added according tests

--- js/arrayProcessingTool.js
+++ js/arrayProcessingTool.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 var ARRAY_PROCESSING_TOOL = (function () {
 
-	var arraySample = [-1, 2, 3, -9];
+	var arraySample = [-1, 2, 3, 4, -9, 5];
 
 	return {
 		arraySample: arraySample,
@@ -19,6 +19,26 @@ var ARRAY_PROCESSING_TOOL = (function () {
 			}
 
 			return maxSum;
+		},
+		getMaxValue: function (array) {
+			return Math.max.apply(null, array);
+		},
+		getMediumValue: function (array) {
+			array.sort(function (a, b) {
+				return a - b;
+			});
+			var median = function () {
+				if (array.length % 2 === 0) {
+					return (array[array.length / 2] + array[array.length / 2 - 1]) / 2;
+				} else {
+					return array[Math.floor(array.length / 2)];
+				}
+			};
+			return median();
+		},
+		getMinValue: function (array) {
+			return Math.min.apply(null, array);
+
 		}
 	};
 }());
--- js/test1.js
+++ js/test1.js
@@ -4,15 +4,42 @@ describe('getMaxSubSum function', function () {
 	it('should be a function', function () {
 		expect(ARRAY_PROCESSING_TOOL.getMaxSubSum).to.be.a.Function;
 	});
-	
+
 	it('should return expected result', function () {
 		var testArray = [-2, -1, 1, 2];
 		chai.assert.equal(ARRAY_PROCESSING_TOOL.getMaxSubSum(testArray), 3, 'numbers are equal');
 	});
-	
-	it('should return 0 if array consists of negative numbers',function () {
-		var arrayOfNegativeNumbers = [-1,-2,-4];
+
+	it('should return 0 if array consists of negative numbers', function () {
+		var arrayOfNegativeNumbers = [-1, -2, -4];
 		chai.assert.equal(ARRAY_PROCESSING_TOOL.getMaxSubSum(arrayOfNegativeNumbers), 0, 'numbers are equal');
 	});
-	
+
+});
+describe('get Max/Medium/Min value in array functions', function () {
+	it('should be a function', function () {
+		expect(ARRAY_PROCESSING_TOOL.getMaxValue).to.be.a.Function;
+	});
+	it('should be a function', function () {
+		expect(ARRAY_PROCESSING_TOOL.getMediumValue).to.be.a.Function;
+	});
+	it('should be a function', function () {
+		expect(ARRAY_PROCESSING_TOOL.getMinValue).to.be.a.Function;
+	});
+	it('should return right median value if array.length is even', function () {
+		var arrayToGetMediumValEven = [2, 6, -1, 5, -4, 8];
+		chai.assert.equal(ARRAY_PROCESSING_TOOL.getMediumValue(arrayToGetMediumValEven), 3.5, 'numbers are equal');
+	});
+	it('should return right median value if array.length is odd', function () {
+		var arrayToGetMediumValOdd = [6, -7, 3, 5, 8];
+		chai.assert.equal(ARRAY_PROCESSING_TOOL.getMediumValue(arrayToGetMediumValOdd), 5, 'numbers are equal');
+	});
+	it('should return Min value', function () {
+		var arrayToGetMinValue = [-3, -5, 5, -7];
+		chai.assert(ARRAY_PROCESSING_TOOL.getMinValue(arrayToGetMinValue), -2, 'numbers are equal')
+	});
+	it('should return Max value', function () {
+		var arrayToGetMaxValue = [-3, -35, 5, -70, 12];
+		chai.assert(ARRAY_PROCESSING_TOOL.getMinValue(arrayToGetMaxValue), -2, 'numbers are equal')
+	});
 });
