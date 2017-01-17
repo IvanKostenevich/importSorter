3a76b7f50d90158c00afc71401b726834a64a31d
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-17 13:14:36 +0300

    made add function according to task

--- src/task_2.js
+++ src/task_2.js
@@ -1,40 +1,14 @@
 'use strict';
 
-// eslint-disable-next-line no-unused-vars
-var ADDITION_MODULE = (function () {
 
-	return {
+var add = function (a, b) {
+	if (typeof b === 'undefined') {
+		return function (y) {
+			return a + y;
+		};
+	}
+	return a + b;
 
-		addTwoArguments: function (a, b) {
-			if (typeof b === 'undefined') {
-				return function (y) {
-					return a + y;
-				};
-			}
-			return a + b;
-
-		},
-		addMultipleArguments: function (a) {
-
-			var sum = a;
-
-			function addAndRepeat(b) {
-				if (b) {
-					sum += b;
-					return addAndRepeat;
-				}
-				else {
-					return sum;
-				}
-			}
-
-			addAndRepeat.toString = function () {
-				return sum;
-			};
-
-			return addAndRepeat;
-		}
-	};
-}());
-
-
+};
+add(3,4);
+add(3)(4);
--- src/task_2_test.js
+++ src/task_2_test.js
@@ -2,12 +2,9 @@
 
 describe('ADDITION_MODULE',function () {
 	it('should add 2 arguments with addTwoArguments(x, y) syntax',function () {
-		expect(ADDITION_MODULE.addTwoArguments(5,6)).to.be.eql(11);
+		expect(add(5,6)).to.be.eql(11);
 	});
 	it('should add 2 arguments with addTwoArguments(x)(y) syntax',function () {
-		expect(ADDITION_MODULE.addTwoArguments(10)(6)).to.be.eql(16);
-	});
-	it('shuld add multiple arguments with addMultipleArguments(x)(y)(z)',function () {
-		expect(ADDITION_MODULE.addMultipleArguments(9)(6)(5)).to.be.eql(20);
+		expect(add(10)(6)).to.be.eql(16);
 	});
 });
