d95ea0a50f8ad59ace1f447d7cf5f7478ca53f7c
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-19 15:51:07 +0300

    completed task_4,added tests for task_4

---
+++ src/task_4.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+var splitNumberIntoDigitList = function (number) {
+	var arrayOfDigits = [];
+	var stringArray = number.toString().split('');
+	for (var i = 0; i < stringArray.length; i++) {
+		arrayOfDigits.push(parseInt(stringArray[i]));
+	}
+	return arrayOfDigits;
+};
+
+// eslint-disable-next-line no-unused-vars
+var splitNumberIntoDigitList = function (number) {
+	var arrayOfDigits = [];
+	var stringArray = number.toString().split('');
+	for (var i = 0; i < stringArray.length; i++) {
+		arrayOfDigits.push(parseInt(stringArray[i]));
+	}
+	return arrayOfDigits;
+};
---
+++ src/task_4_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('splitNumberIntoDigitList function', function () {
+	it('should split as expected', function () {
+		var number = 123456;
+		expect(splitNumberIntoDigitList(number)).to.be.eql([1, 2, 3, 4, 5, 6]);
+	});
+	it('number.length should be equal to array.length',function () {
+		var number = 123456;
+		expect(splitNumberIntoDigitList(number).length).to.be.eql(number.toString().length);
+	});
+});
