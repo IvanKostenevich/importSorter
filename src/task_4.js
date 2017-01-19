cd69a5b3c93154df719b5a65ea06882d6ad65c77
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-19 16:37:53 +0300

    optimized 4th task

--- src/task_4.js
+++ src/task_4.js
@@ -1,21 +1,19 @@
 'use strict';
 
-// eslint-disable-next-line no-unused-vars
-var splitNumberIntoDigitList = function (number) {
-	var arrayOfDigits = [];
-	var stringArray = number.toString().split('');
-	for (var i = 0; i < stringArray.length; i++) {
-		arrayOfDigits.push(parseInt(stringArray[i]));
-	}
-	return arrayOfDigits;
-};
+// var splitNumberIntoDigitList = function (number) {
+// 	var arrayOfDigits = [];
+// 	var stringArray = number.toString().split('');
+// 	for (var i = 0; i < stringArray.length; i++) {
+// 		arrayOfDigits.push(parseInt(stringArray[i]));
+// 	}
+// 	return arrayOfDigits;
+// };
 
 // eslint-disable-next-line no-unused-vars
 var splitNumberIntoDigitList = function (number) {
-	var arrayOfDigits = [];
 	var stringArray = number.toString().split('');
-	for (var i = 0; i < stringArray.length; i++) {
-		arrayOfDigits.push(parseInt(stringArray[i]));
-	}
-	return arrayOfDigits;
+	return stringArray.map(function (a) {
+		return parseInt(a);
+	});
 };
+
