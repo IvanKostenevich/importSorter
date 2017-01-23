e5289f291308345e31bffe5946670eba9876cabd
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-23 16:45:47 +0300

    added 9th task

--- src/task_6.js
+++ src/task_6.js
@@ -22,6 +22,18 @@ function map(array, callback, thisArg) {
 }
 
 // eslint-disable-next-line no-unused-vars
+function find(array, callback, thisArg) {
+
+	for (var i = 0; i < array.length; i++) {
+
+		if (callback.call(thisArg, array[i], i, array) === true) {
+			
+			return array[i];
+		}
+	}
+}
+
+// eslint-disable-next-line no-unused-vars
 function filter(array, callback, thisArg) {
 	var finalArray = [];
 
