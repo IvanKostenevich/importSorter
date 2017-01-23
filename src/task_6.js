6dcc9f506cd617da90e65464a1a3856f3403d2a4
IvanKostenevich <ivan.kostenevich91@gmail.com>
2017-01-23 16:26:07 +0300

    added 7th and 8th task

--- src/task_6.js
+++ src/task_6.js
@@ -1,10 +1,36 @@
 'use strict';
 
 // eslint-disable-next-line no-unused-vars
-function each (array, callback, thisArg) {
+function each(array, callback, thisArg) {
 
 	for (var i = 0; i < array.length; i++) {
 		callback.call(thisArg, array[i], i, array);
 	}
-	
+
+}
+
+// eslint-disable-next-line no-unused-vars
+function map(array, callback, thisArg) {
+	var finalArray = [];
+
+	for (var i = 0; i < array.length; i++) {
+
+		finalArray.push(callback.call(thisArg, array[i], i, array));
+	}
+
+	return finalArray;
+}
+
+// eslint-disable-next-line no-unused-vars
+function filter(array, callback, thisArg) {
+	var finalArray = [];
+
+	for (var i = 0; i < array.length; i++) {
+
+		if (callback.call(thisArg, array[i], i, array) === true) {
+
+			finalArray.push(array[i]);
+		}
+	}
+	return finalArray;
 }
